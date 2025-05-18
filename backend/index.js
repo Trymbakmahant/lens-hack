c7b289e62c51799ab2f7c772f65469c2711ca2de
sliponit <sliponit@xps13>
2025-05-18 09:30:40 +0200

    chore: call args

--- backend/index.js
+++ backend/index.js
@@ -71,5 +71,5 @@ async function postGame(gameId, groveId, randomUuid) {
 (async () => {
   const gameId = '8370814a0f6f3402129eb775d3379498c42d';
   const groveId = 'a32b6580bc2b869e6ab855a5cf35062499d1e66422bd7f41ea8794d78b01c114';
-  await postGame(gameId, groveId);
+  await postGame(gameId, groveId, null);
 })();
